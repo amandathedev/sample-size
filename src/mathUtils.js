b9bc214c3813cfc13fb20d733d740b48ea97acd0
Amanda Treutler <amandathedev@gmail.com>
2024-09-02 19:48:45 -0500

    working formula

--- src/mathUtils.js
+++ src/mathUtils.js
@@ -9,25 +9,18 @@ export function calculateSampleSize(baselineRate, minDetectableEffect, option, p
 
   // Calculate Z-scores based on the significance level and power
   const zAlphaOver2 = jstat.normal.inv(1 - significanceLevel / 2, 0, 1);
-  // Calculate the Z-score for the statistical power (1 - beta).
   const zBeta = jstat.normal.inv(power, 0, 1);
 
-  // Calculate the effect size (delta) based on the option
-  const delta = option === 'Absolute' ? minDetectableEffect : baselineRate * minDetectableEffect;
+  // Baseline conversion rate (p)
+  const p = baselineRate > 0.5 ? 1.0 - baselineRate : baselineRate;
 
-  // Baseline conversion rate (p1)
-  const p1 = baselineRate;
-
-  // Conversion rate after applying the effect (p2)
-  const p2 = p1 + delta;
-
-  // Calculate variance for p1 and p2
-  const varianceP1 = p1 * (1 - p1);
-  const varianceP2 = p2 * (1 - p2);
+  // Calculate standard deviations
+  const sd1 = Math.sqrt(2 * p * (1 - p));
+  const sd2 = Math.sqrt(p * (1 - p) + (p + minDetectableEffect) * (1 - p - minDetectableEffect));
 
   // Calculate the numerator and denominator for the sample size formula
-  const numerator = Math.pow(zAlphaOver2 + zBeta, 2) * (varianceP1 + varianceP2);
-  const denominator = Math.pow(delta, 2);
+  const numerator = Math.pow(zAlphaOver2 * sd1 + zBeta * sd2, 2);
+  const denominator = Math.pow(minDetectableEffect, 2);
 
   // Calculate and return the sample size
   const sampleSize = numerator / denominator;
